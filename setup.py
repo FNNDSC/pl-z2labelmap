eda18feda60e5c7106b89f605f2e165f6843fd01
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2019-03-24 16:38:00 -0400

    Bump to 1.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 setup(
       name             =   'z2labelmap',
-      version          =   '1.1.1',
+      version          =   '1.1.2',
       description      =   'Convert lh/rh z-score vector to FreeSurfer labelmap', 
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
